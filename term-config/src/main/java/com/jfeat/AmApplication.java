db7fc721e23259e9bf42837954db13b93658c715
m.2@sandbox <m.2@sandbox>
2022-01-12 10:30:37 +0800

    remove SqlInjector

--- term-config/src/main/java/com/jfeat/AmApplication.java
+++ term-config/src/main/java/com/jfeat/AmApplication.java
@@ -1,9 +1,7 @@
 package com.jfeat;
 
-import com.jfeat.crud.core.properties.AmProperties;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -31,6 +29,6 @@ public class AmApplication extends WebMvcConfigurerAdapter{
 
     public static void main(String[] args) {
         SpringApplication.run(AmApplication.class, args);
-        logger.info("SB TERMCONFIG is success!");
+        logger.info("TERM-CONFIG is success!");
     }
 }
