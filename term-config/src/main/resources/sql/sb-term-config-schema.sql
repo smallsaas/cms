caeb99a4f7189567f4622b34b092956c453f8182
Vincent <a30f49@gmail.com>
2022-02-15 22:32:52 +0800

    Update sb-term-config-schema.sql

--- term-config/src/main/resources/sql/sb-term-config-schema.sql
+++ term-config/src/main/resources/sql/sb-term-config-schema.sql
@@ -4,9 +4,10 @@ CREATE TABLE `t_term_config` (
 	type VARCHAR (50) NOT NULL COMMENT '类型 ABOUT_US,NOTICE,CONNECT_US ...',
 	title VARCHAR (50) NOT NULL COMMENT '标题',
 	content text COMMENT '内容',
-	`org_id` bigint(20) DEFAULT NULL COMMENT '组织(部门)ID',
+	org_id bigint(20) DEFAULT NULL COMMENT '组织(部门)ID',
 	created_time DATETIME NULL COMMENT '创建时间',
+        create_time DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
 	previous_modified_time DATETIME NULL COMMENT '上次修改时间',
-	last_modified_time DATETIME NULL COMMENT '最后修改时间',
+	last_modified_time DATETIME NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '最后修改时间',
 	PRIMARY KEY (`id`)
 ) ENGINE = INNODB DEFAULT CHARSET = UTF8;
