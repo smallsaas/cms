8769bd7736e72388111e947f1e2080f359776c2b
ye126 <jiajiajia6000@126.com>
2022-08-12 14:49:19 +0800

    feedback调整

--- feedback-service/src/main/java/com/jfeat/module/feedback/api/CloudFeedBackEndpoint.java
+++ feedback-service/src/main/java/com/jfeat/module/feedback/api/CloudFeedBackEndpoint.java
@@ -8,11 +8,15 @@ import com.jfeat.crud.base.exception.BusinessException;
 import com.jfeat.crud.base.tips.SuccessTip;
 import com.jfeat.crud.base.tips.Tip;
 import com.jfeat.module.feedback.services.gen.persistence.dao.CMSFeedbackMapper;
+import com.jfeat.module.feedback.services.gen.persistence.model.FBComplainRequestType;
 import com.jfeat.module.smallsaas.ticket.api.FeedbackEndpoint;
 import com.jfeat.module.smallsaas.ticket.api.request.CreateFeedBackRequest;
+import com.jfeat.module.smallsaas.ticket.services.domain.converter.ComplainConverter;
 import com.jfeat.module.smallsaas.ticket.services.domain.dao.QueryComplainRecordDao;
+import com.jfeat.module.smallsaas.ticket.services.domain.service.ComplainRecordService;
 import com.jfeat.module.smallsaas.ticket.services.gen.persistence.dao.ComplainRecordMapper;
 import com.jfeat.module.smallsaas.ticket.services.gen.persistence.model.complainrecord.ComplainRecord;
+import com.jfeat.module.smallsaas.ticket.services.gen.persistence.model.complainrecord.ComplainRequestType;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.*;
@@ -27,29 +31,47 @@ public class CloudFeedBackEndpoint {
     @Resource
     FeedbackEndpoint feedbackEndpoint;
 
+    @Resource
+    ComplainRecordService complainRecordService;
+
 
     @Resource
     QueryComplainRecordDao queryComplainRecordDao;
 
     @ApiOperation(value = "用户进行反馈")
-    @PostMapping("/feedback")
-    public Tip feedbackPage(@RequestBody CreateFeedBackRequest request) {
+    @PostMapping("/feedback/{type}")
+    public Tip feedback(@RequestBody CreateFeedBackRequest request,@PathVariable String type) {
         if (JWTKit.getUserId() == null) {
             throw new BusinessException(BusinessCode.NoPermission, "用户未登录");
         }
         request.setComplainantId(JWTKit.getUserId());
-        return feedbackEndpoint.createFeedbackRecord(request);
+        complainRecordService.createComplain(ComplainConverter.COMPLAIN.toCommand(request, type));
+        return SuccessTip.create();
     }
+
+    @ApiOperation(value = "用户进行反馈")
+    @PostMapping("/feedback")
+    public Tip feedbackType(@RequestBody CreateFeedBackRequest request) {
+        if (JWTKit.getUserId() == null) {
+            throw new BusinessException(BusinessCode.NoPermission, "用户未登录");
+        }
+        request.setComplainantId(JWTKit.getUserId());
+        complainRecordService.createComplain(ComplainConverter.COMPLAIN.toCommand(request, FBComplainRequestType.FEEDBACK.name()));
+        return SuccessTip.create();
+    }
+
+
 
     @ApiOperation(value = "反馈列表")
     @GetMapping("/feedback")
-    public Tip feedback(Page page,
+    public Tip feedbackPage(Page page,
                         @RequestParam(name = "pageNum", required = false, defaultValue = "1") Integer pageNum,
-                        @RequestParam(name = "pageSize", required = false, defaultValue = "5") Integer pageSize) {
+                        @RequestParam(name = "pageSize", required = false, defaultValue = "5") Integer pageSize,
+                        @RequestParam(name = "requestType", required = false, defaultValue = "5") String requestType) {
 
         page.setCurrent(pageNum);
         page.setSize(pageSize);
-        page.setRecords(queryComplainRecordDao.queryComplainRecordPage(page,null));
+        page.setRecords(queryComplainRecordDao.queryComplainRecordPage(page,requestType));
 
         return SuccessTip.create(page);
     }
---
+++ feedback-service/src/main/java/com/jfeat/module/feedback/services/gen/persistence/model/FBComplainRequestType.java
@@ -0,0 +1,12 @@
+package com.jfeat.module.feedback.services.gen.persistence.model;
+
+
+public enum FBComplainRequestType {
+    FEEDBACK,  // 投诉反馈
+    ORDER_DISPUTES,
+    PROPOSAL; // 建议
+
+    private FBComplainRequestType() {
+    }
+}
+
