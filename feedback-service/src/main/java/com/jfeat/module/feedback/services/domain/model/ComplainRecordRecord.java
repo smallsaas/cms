dcf5709093cc13f4ab68223fd5d8535f9cf9a2c2
ye126 <jiajiajia6000@126.com>
2022-08-13 14:53:35 +0800

    增加字段回显

--- feedback-service/src/main/java/com/jfeat/module/feedback/services/domain/model/ComplainRecordRecord.java
+++ feedback-service/src/main/java/com/jfeat/module/feedback/services/domain/model/ComplainRecordRecord.java
@@ -7,5 +7,22 @@ import com.jfeat.module.feedback.services.gen.persistence.model.ComplainRecord;
  * Created by Code generator on 2022-08-12
  */
 public class ComplainRecordRecord extends ComplainRecord {
-    
+    private String userName;
+    private String userPhone;
+
+    public String getUserName() {
+        return userName;
     }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserPhone() {
+        return userPhone;
+    }
+
+    public void setUserPhone(String userPhone) {
+        this.userPhone = userPhone;
+    }
+}
