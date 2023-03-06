d0a7442d366d8f5db6c9ca02d8d75dff3f98769d
hdj <263463764@qq.com>
2023-03-06 11:04:49 +0800

    default

--- rss/src/main/java/com/jfeat/AmApplication.java
+++ rss/src/main/java/com/jfeat/AmApplication.java
@@ -9,6 +9,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AmApplication {
     public static void main(String[] args) {
         SpringApplication.run(AmApplication.class, args);
-        log.info("Application run success!");
+        //log.info("Application run success!");
     }
 }
